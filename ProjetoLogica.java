054b677124726dcf49fe554d314fe84d1092d60d
Iris Soares <iss3@cin.ufpe.br>
2018-10-20 15:16:01 -0300

    Projeto Final

--- ProjetoLogica.java
+++ ProjetoLogica.java
@@ -20,27 +20,29 @@ public class ProjetoLogica {
 			for (int q = 1; q <= n; q++) { // lê todas as linhas
 				
 				out.write("Problema #" + q + "\n");
-				String eV = in.nextLine();
+				String line = in.nextLine();
 				int i;
 
-				for (i = 0; i < eV.length() && // separa a expressao da valoração
-						(eV.charAt(i) != '1' && eV.charAt(i) != '0'); i++) {
-				}
-				if (i == eV.length()) {
-					if (eV.charAt(0) == '{') {
-						out.write("Ha uma palavra nao legitima no conjunto.​\n");
-					} else {
-						out.write("A palavra nao e legitima.\n");
+				for (i = 0; i < line.length() && (line.charAt(i) != '1' && line.charAt(i) != '0'); i++) {
+				} // separar a expressao da valoração
+
+				if (i == line.length()) { //caso não tenham as valorações na entrada
+					if (line.charAt(0) == '{') {
+						out.write("Ha uma palavra nao legitima no conjunto.\n\n");
+					} 
+					else {
+						out.write("A palavra nao e legitima.\n\n");
 					}
-				} else {
-					String expressao = eV.substring(0, (i - 1));
-					String valoracao = eV.substring(i, (eV.length()));
+				} 
+				else {
+					String expressao = line.substring(0, (i - 1));
+					String valoracao = line.substring(i, (line.length()));
 
 					String[] valoracoesAux = valoracao.split(" "); // faz um array de int com as valorações
 					boolean[] valoracoes = new boolean[valoracoesAux.length];
 					char[] variaveis = new char[valoracoesAux.length];
 
-					for (int u = 0; u < valoracoes.length; u++) {
+					for (int u = 0; u < valoracoes.length; u++) { //passa as valorações pra boolean e salva no array
 						int aux = Integer.parseInt(valoracoesAux[u]);
 						if (aux == 0) {
 							valoracoes[u] = false;
@@ -49,45 +51,52 @@ public class ProjetoLogica {
 						}
 					}
 					int indexA = 0;
-					for (int o = 0; o < expressao.length(); o++) { // fazer correspondencia das variaveis e sua
-																	// valoração
+					for (int o = 0; o < expressao.length(); o++) { // faz a correspondencia das variaveis e sua valoração no hashmap
+																	
 						if (expressao.charAt(o) >= 'A' && expressao.charAt(o) <= 'Z' && indexA < variaveis.length) {
 							if (funcaoA(expressao.charAt(o), variaveis, 0)) {
-								variaveis[indexA] = expressao.charAt(o);
+								variaveis[indexA] = expressao.charAt(o); //salva as variaveis em um array pra chechar se já tá salvo
 								valoresverdade.put(variaveis[indexA], valoracoes[indexA]);
-								indexA++; // só tenho que ver se da merda nessa funçao comparar um char com o array
-											// vazio (nem deu merda OBRIGADA DEUX)
+								indexA++; 
 							}
 						}
 					}
-					if (expressao.charAt(0) == '{') { // Conjunto de expressões
+					if (expressao.charAt(0) == '{') { // conjunto de expressões
 						String splitarExpressoes = expressao.substring(1, expressao.length() - 1);
 						String[] expressoes = splitarExpressoes.split(", ");
 
-						for (int h = 0; h < expressoes.length; h++) {//tenho que ver todas as expressoes primeiro e dar prioridade pra ser nao legitima > insat > sat
+						int cont = 0;
+						for (int h = 0; h < expressoes.length; h++) {
 							boolean legit = isLegit(expressoes[h]);
 							if (!legit) {
-								out.write("Ha uma palavra nao legitima no conjunto.​\n\n");
+								out.write("Ha uma palavra nao legitima no conjunto.\n\n");
 								h = expressoes.length;
-							} else {
+							} 
+							else {
 								boolean satisfaz = satisfies(valoresverdade, expressoes[h]);
 								if (!satisfaz) {
+									cont++;
+								} 
+								if (h == expressoes.length - 1 && cont > 0) {
 									out.write("A valoracao-verdade nao satisfaz o conjunto.\n\n");
-									h = expressoes.length;
-								} else if (satisfaz && h == expressoes.length - 1) {
+								}
+								else if (satisfaz && h == expressoes.length - 1) {
 									out.write("A valoracao-verdade satisfaz o conjunto.\n\n");
 								}
 							}
 						}
-					} else { // Apenas uma expressão
+					} 
+					else { // Apenas uma expressão
 						boolean legit = isLegit(expressao);
 						if (!legit) {
 							out.write("A palavra nao e legitima.\n\n");
-						} else {
+						} 
+						else {
 							boolean satisfaz = satisfies(valoresverdade, expressao);
 							if (satisfaz) {
 								out.write("A valoracao-verdade satisfaz a proposicao.\n\n");
-							} else {
+							} 
+							else {
 								out.write("A valoracao-verdade nao satisfaz a proposicao.\n\n");
 							}
 						}
@@ -96,14 +105,16 @@ public class ProjetoLogica {
 			}
 			out.close();
 			in.close();
-		} catch (IOException e) {
+		} 
+		catch (IOException e) {
 		}
 	}
 
 	static boolean funcaoA(char charAt, char[] variaveis, int indexA) {
 		if (variaveis.length - 1 == indexA && variaveis[indexA] != charAt) {
 			return true;
-		} else if (variaveis[indexA] == charAt) {
+		} 
+		else if (variaveis[indexA] == charAt) {
 			return false;
 		}
 		return funcaoA(charAt, variaveis, indexA + 1);
@@ -133,13 +144,13 @@ public class ProjetoLogica {
 
 			if (exp.charAt(i) == '(')
 				numParenthesis++;
+			
 			else if (exp.charAt(i) == ')')
 				numParenthesis--;
 
 			if ((exp.charAt(i) == '>' || exp.charAt(i) == '&' || exp.charAt(i) == 'v' || exp.charAt(i) == '~') && numParenthesis == 1)
 				return i;
 		}
-
 		return -1;
 	}
 
